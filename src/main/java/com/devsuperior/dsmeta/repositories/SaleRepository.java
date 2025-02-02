d9cce02bfcf76c98c478629be03517ff7fccdd50
Julio Cesar <jccintr@gmail.com>
2025-02-02 01:08:14 -0300

    Testado o comando sql de cada relatorio

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -7,3 +7,26 @@ import com.devsuperior.dsmeta.entities.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
 }
+
+/*
+ 
+ RELATORIO DE VENDAS 
+ 
+select tb_sales.id,tb_sales.dt_sale,tb_sales.amount,tb_seller.name from tb_sales
+inner join tb_seller
+on tb_seller.id = tb_sales.seller_id
+where  UPPER(tb_seller.name) LIKE UPPER('%odinson%')
+and
+tb_sales.dt_sale between '2022-05-01' and '2022-05-31'
+
+*/
+
+/*
+ Sumário de vendas por vendedor 
+ 
+ SELECT tb_seller.name, SUM(tb_sales.amount) from tb_seller
+inner join tb_sales
+on tb_seller.id = tb_sales.seller_id
+where tb_sales.dt_sale between '2022-01-01' and '2022-06-30'
+group by tb_seller.name
+*/
